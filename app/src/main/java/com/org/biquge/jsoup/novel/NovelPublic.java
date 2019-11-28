2c29f8da34a99f7ba026504af5e6e4d7ba6bf0bf
kun.tang <kun.tang@synwing.com>
2019-11-28 16:34:46 +0800

    图标修改

--- app/src/main/java/com/org/biquge/jsoup/novel/NovelPublic.java
+++ app/src/main/java/com/org/biquge/jsoup/novel/NovelPublic.java
@@ -17,7 +17,7 @@ import javax.net.ssl.X509TrustManager;
 public class NovelPublic {
     public static String novelHomeUrl = "http://www.xbiquge.la";//笔趣阁
     public static String novelsContent = "http://www.xbiquge.la/xiaoshuodaquan/";
-    public static String novelSaveDirName = "/小说";
+    public static String novelSaveDirName = "/freeTime";
     public static String downLoadingUpdata = "novelLoadingUpdata";
     public static String novelHomeUrl2 = "http://book.zongheng.com/store/c0/c0/b0/u0/p2/v9/s9/t0/u0/i1/ALL.html";//纵横中文网
     public static String biqudao = "https://www.biqudao.com/";//笔趣岛
