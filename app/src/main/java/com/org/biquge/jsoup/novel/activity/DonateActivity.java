413ca5a9b630d34091ab05e5f48642f330eb660f
kun.tang <kun.tang@synwing.com>
2020-03-20 10:19:32 +0800

    捐赠页面

--- app/src/main/java/com/org/biquge/jsoup/novel/activity/DonateActivity.java
+++ app/src/main/java/com/org/biquge/jsoup/novel/activity/DonateActivity.java
@@ -1,12 +1,18 @@
 package com.org.biquge.jsoup.novel.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.githang.statusbar.StatusBarCompat;
 import com.org.biquge.jsoup.MyPreference;
 import com.org.biquge.jsoup.R;
+import com.org.biquge.jsoup.novel.NovelPublic;
 import com.org.biquge.jsoup.novel.events.RefreshTheme;
 
 import org.greenrobot.eventbus.EventBus;
@@ -21,17 +27,31 @@ public class DonateActivity extends AppCompatActivity {
 
     @BindView(R.id.rl_top)
     RelativeLayout rlTop;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.iv_donate)
+    ImageView ivDonate;
     private MyPreference myPreference;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
         ButterKnife.bind(this);
+        context = this;
         EventBus.getDefault().register(this);
         myPreference = MyPreference.getInstance();
         myPreference.setPreference(this);
         refreshTheme(new RefreshTheme());
+        initDonateIv();
+    }
+
+    private void initDonateIv() {
+        Glide.with(context)
+                .load("https://raw.githubusercontent.com/LqTk/mynovel/master/app/src/main/res/drawable/donate.png")
+                .apply(NovelPublic.errorOptions())
+                .into(ivDonate);
     }
 
 
